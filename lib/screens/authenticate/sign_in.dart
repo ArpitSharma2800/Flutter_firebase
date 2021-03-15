6e43649fb9298665d5e9bdb3a365f0d7dd0157c9
ArpitSharma2800 <51937952+ArpitSharma2800@users.noreply.github.com>
2021-03-15 18:05:25 +0530

    form creation

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -31,30 +31,32 @@ class _SignInState extends State<SignIn> {
             builder: (context, provider, child) {
           return SingleChildScrollView(
             child: Container(
-              constraints: BoxConstraints(
-                  maxHeight: MediaQuery.of(context).size.height,
-                  maxWidth: MediaQuery.of(context).size.width),
-              decoration: BoxDecoration(color: Colors.black),
-              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-              child: TextButton(
-                child: Text('sign in anon'),
-                onPressed: () async {
-                  dynamic result = await _auth.signInAnon();
-                  if (result == null) {
-                    print('error signing in');
-                  } else {
-                    provider.authCall(result.email,
-                        result.metadata.lastSignInTime, result.uid);
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => HomeApp()),
-                    );
-                    print('signed in');
-                    print(result.metadata.lastSignInTime);
-                  }
-                },
-              ),
-            ),
+                constraints: BoxConstraints(
+                    maxHeight: MediaQuery.of(context).size.height,
+                    maxWidth: MediaQuery.of(context).size.width),
+                decoration: BoxDecoration(color: Colors.black),
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Hello\nWelcome Back!!',
+                        style: TextStyle(
+                            color: Colors.white54,
+                            fontSize: 26.0,
+                            fontWeight: FontWeight.w700)),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    const Divider(
+                      color: Colors.white54,
+                      height: 5,
+                      thickness: 2,
+                      indent: 0,
+                      endIndent: 0,
+                    ),
+                  ],
+                )),
           );
         }),
       ),
