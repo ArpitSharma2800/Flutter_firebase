3778f280fa90d388f7f5f3c0c4062bc7fc94174f
ArpitSharma2800 <51937952+ArpitSharma2800@users.noreply.github.com>
2021-03-15 17:45:48 +0530

    Welcome Screen UI

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -11,40 +11,69 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  final myControllerPassword = TextEditingController();
+  final myControllerEmail = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[100],
-      appBar: AppBar(
-        backgroundColor: Colors.brown[400],
-        elevation: 0.0,
-        title: Text('Sign in to Brew Crew'),
+    return SafeArea(
+      child: Scaffold(
+        key: _scaffoldKey,
+        backgroundColor: Colors.black,
+        // appBar: AppBar(
+        //   // backgroundColor: Colors.brown[400],
+        //   elevation: 0.0,
+        //   title: Text('Login to 100DaysOfCode'),
+        // ),
+        body: Consumer<FirebaseAuthenticated>(
+            builder: (context, provider, child) {
+          return SingleChildScrollView(
+            child: Container(
+              constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.height,
+                  maxWidth: MediaQuery.of(context).size.width),
+              decoration: BoxDecoration(color: Colors.black),
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: TextButton(
+                child: Text('sign in anon'),
+                onPressed: () async {
+                  dynamic result = await _auth.signInAnon();
+                  if (result == null) {
+                    print('error signing in');
+                  } else {
+                    provider.authCall(result.email,
+                        result.metadata.lastSignInTime, result.uid);
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => HomeApp()),
+                    );
+                    print('signed in');
+                    print(result.metadata.lastSignInTime);
+                  }
+                },
+              ),
+            ),
+          );
+        }),
       ),
-      body:
-          Consumer<FirebaseAuthenticated>(builder: (context, provider, child) {
-        return Container(
-          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-          child: TextButton(
-            child: Text('sign in anon'),
-            onPressed: () async {
-              dynamic result = await _auth.signInAnon();
-              if (result == null) {
-                print('error signing in');
-              } else {
-                provider.authCall(
-                    result.email, result.metadata.lastSignInTime, result.uid);
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomeApp()),
-                );
-                print('signed in');
-                print(result.metadata.lastSignInTime);
-              }
-            },
-          ),
-        );
-      }),
     );
   }
 }
+
+// onPressed: () async {
+//                   dynamic result = await _auth.signInAnon();
+//                   if (result == null) {
+//                     print('error signing in');
+//                   } else {
+//                     provider.authCall(result.email,
+//                         result.metadata.lastSignInTime, result.uid);
+//                     Navigator.pushReplacement(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => HomeApp()),
+//                     );
+//                     print('signed in');
+//                     print(result.metadata.lastSignInTime);
+//                   }
+//                 },
